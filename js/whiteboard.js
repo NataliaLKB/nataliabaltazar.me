14da75c58844fa415794c2ad1dd7860e84daba92
NataliaLKB <natalialeebaltazar@gmail.com>
2015-02-01 15:15:39 +0000

    added buttons to change colour

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -1,64 +1,65 @@
-(function () {
-    "use strict";
+var canvas,
+    stage,
+    plots = [],
+    isActive = false,
+    // Declaring colours
+    blue = "#00FFFF",
+    pink = "#FF00FF",
+    white = "#FFFFFF",
+    black = "#000000",
+    yellow = "#FFFF00",
+    colour = blue;
 
-    var canvas,
-        stage,
-        plots = [],
-        isActive = false,
-        // Declaring colours
-        blue = "#00FFFF",
-        pink = "#FF00FF",
-        white = "#FFFFFF",
-        black = "#000000",
-        yellow = "#FFFF00";
+function init (){
+    canvas = document.getElementById('canvas'); 
 
-    function init (){
-        canvas = document.getElementById('canvas'); 
+    stage = canvas.getContext('2d');
+    stage.lineWidth = '3';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-        stage = canvas.getContext('2d');
-        stage.lineWidth = '3';
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    canvas.addEventListener('mousedown', startDraw, false);
+    canvas.addEventListener('mousemove', draw, false);
+    canvas.addEventListener('mouseup', endDraw, false);
+}
 
-        canvas.addEventListener('mousedown', startDraw, false);
-        canvas.addEventListener('mousemove', draw, false);
-        canvas.addEventListener('mouseup', endDraw, false);
-    }
-    
-    function startDraw(e) { 
-        isActive = true; 
-    }   
+function changeColour (color) {
+    colour = color;
+}
 
-    function draw(e) { 
-        if(!isActive) return;   
-        // cross-browser canvas coordinates
-        var x = e.offsetX || e.layerX - canvas.offsetLeft; 
-        var y = e.offsetY || e.layerY - canvas.offsetTop;   
+function startDraw(e) { 
+    isActive = true; 
+}   
 
-        plots.push({x: x, y: y}); 
+function draw(e) { 
+    if(!isActive) return;   
+    // cross-browser canvas coordinates
+    var x = e.offsetX || e.layerX - canvas.offsetLeft; 
+    var y = e.offsetY || e.layerY - canvas.offsetTop;   
 
-        //declares what colour for drawing
-        drawOnCanvas(blue, plots); 
-    }
+    plots.push({x: x, y: y}); 
 
-    function drawOnCanvas(color, plots) { 
-        stage.beginPath();
-        stage.strokeStyle = color;
-        stage.moveTo(plots[0].x, plots[0].y);   
-        for(var i=1; i<plots.length; i++) { 
-          stage.lineTo(plots[i].x,
-           plots[i].y); 
-        } 
-        stage.stroke(); 
-    }
+    //declares what colour for drawing
+    drawOnCanvas(colour, plots); 
+}
 
-    function endDraw(e) { 
-        isActive = false;   // empty the array 
-        plots = []; 
-    }
+function drawOnCanvas(color, plots) { 
+    stage.beginPath();
+    stage.strokeStyle = color;
+    stage.moveTo(plots[0].x, plots[0].y);   
+    for(var i=1; i<plots.length; i++) { 
+      stage.lineTo(plots[i].x,
+       plots[i].y); 
+    } 
+    stage.stroke(); 
+}
 
-    //initialises whiteboard
-    window.onload = function () {
-    	init();
-    }
-}());
+function endDraw(e) { 
+    isActive = false;   // empty the array 
+    plots = []; 
+}
+
+//initialises whiteboard
+window.onload = function () {
+	init();
+}
